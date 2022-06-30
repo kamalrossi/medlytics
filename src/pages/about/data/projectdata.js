f17f1d5f919912d6e496f59870d696643b809acf
kamalrossi <rossikamal@gmail.com>
2022-07-01 00:27:59 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,48 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "medlytics";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "Medlytics is a responsive web app for analytics based Healthcare managemet.Major modules include  homescreen, services,technology, testimonial, etc."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag3: "JSX",
+    tag4: "MedTech"
+  }
+];
+const projecttime = [
+  {
+    title: "Design",
+    from: "June 2",
+    to: "Aug 12"
+  },
+  {
+    title: "Implementation",
+    from: "Jun 22",
+    to: "Aug 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UI Design",
+    desc:
+      "UX development Requirement analysis and mock prototype of visual graphical representation"
+  },
+  {
+    title: "Responsive Web development",
+    desc:
+      "Coding and implementation  required modules including homescreen, service, technology, testimonial."
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
